ced6cef11e4c042583f7f8af0a5ce4ec89aae3db
BloodStason <bloodstason@gmail.com>
2022-08-23 18:19:49 +0000

    Added ssh public key to wp-app.tf

--- wp-app.tf
+++ wp-app.tf
@@ -20,7 +20,7 @@ resource "yandex_compute_instance" "wp-app-1" {
   }
 
   metadata = {
-#    ssh-keys = "nikandrov:${file("./id_rsa.pub")}"
+    ssh-keys = "nikandrov:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDc39+ovJDrowqxSf2JyrehPW1QCtqOItdB1s4QSSXrlKKS5VdPGgRx+gO4VTMnxJZqxeydQgnn3AJFLq5UMqi/iYkv11ySdNafQs2cItDz7e+KMF2ipgKiYcdhk13g7vbSuGR2SqDKfKUKjXPubC34V2fDagp/uzcuSOSdw3QLbjdp3gIen+zALFto2FWRGnJzFyzlVwOsEXy/qXr9ArmOUj7S8IWqtfXpiDBjljo+iNBZKfyTh5iTHMfcYEA7UANzN1TfGxH9T8BHqW4MfrLbPNOQuGKseKnn3lc3IAsbBbsUByZdAr3XoNfHP1qNbuUed/aUqtgTOMbhjgEjwOwjN7uG0Dev0IxIp3CJQdh+xnBt4m9/mmrxrpZ4F/zNhrfPRV59lD2nXqQulCCKftEbp8Q8qu4FOpddt+CZSlDnpXg8aHGfLAUcjpbDXeYoXX3/ARZbeGseGzNncw8Sl90dd3Gl67ph+u74pkcSMdjVnR9aGsU75bxqERbi1oXn4Ss= nikandrov@packer"
   }
 }
 
@@ -46,6 +46,6 @@ resource "yandex_compute_instance" "wp-app-2" {
   }
 
   metadata = {
-#    ssh-keys = "nikandrov:${file("./ya.pub")}"
+    ssh-keys = "nikandrov:ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDc39+ovJDrowqxSf2JyrehPW1QCtqOItdB1s4QSSXrlKKS5VdPGgRx+gO4VTMnxJZqxeydQgnn3AJFLq5UMqi/iYkv11ySdNafQs2cItDz7e+KMF2ipgKiYcdhk13g7vbSuGR2SqDKfKUKjXPubC34V2fDagp/uzcuSOSdw3QLbjdp3gIen+zALFto2FWRGnJzFyzlVwOsEXy/qXr9ArmOUj7S8IWqtfXpiDBjljo+iNBZKfyTh5iTHMfcYEA7UANzN1TfGxH9T8BHqW4MfrLbPNOQuGKseKnn3lc3IAsbBbsUByZdAr3XoNfHP1qNbuUed/aUqtgTOMbhjgEjwOwjN7uG0Dev0IxIp3CJQdh+xnBt4m9/mmrxrpZ4F/zNhrfPRV59lD2nXqQulCCKftEbp8Q8qu4FOpddt+CZSlDnpXg8aHGfLAUcjpbDXeYoXX3/ARZbeGseGzNncw8Sl90dd3Gl67ph+u74pkcSMdjVnR9aGsU75bxqERbi1oXn4Ss= nikandrov@packer"
   }
 }
